90d1282dc3e82317e9b54c72dfe4c7715d405fca
best <2907647445@qq.com>
2025-03-19 09:23:47 +0800

    构建ComputedRefImpl

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,5 +1,6 @@
 import { isArray } from "@vue/shared"
 import { createDep, Dep } from "./dep"
+import { ComputedRefImpl } from "./computed"
 
 type KeyToDepMap = Map<any, Dep>
 const targetMap = new WeakMap<any, KeyToDepMap>()
@@ -14,11 +15,13 @@ export function effect<T = any>(fn: () => T) {
 export let activeEffect: ReactiveEffect | undefined
 
 export class ReactiveEffect<T = any> {
+    computed?: ComputedRefImpl<T>
+
     constructor(public fn: () => T) { }
 
     run() {
         activeEffect = this
-        this.fn()
+        return this.fn()
     }
 }
 
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -3,3 +3,7 @@ export const isArray = Array.isArray
 export const isObject = (val: unknown) => {
     return val !== null && typeof val === 'object'
 }
+
+export const isFunction = (val: unknown): val is Function => {
+    return typeof val === 'function'
+}
